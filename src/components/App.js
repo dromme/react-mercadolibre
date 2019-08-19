05ced6885d3cd339e04077d85d8c8bd3c7705157
Melissa Barba <melibarba4@gmail.com>
2019-08-19 13:48:54 -0500

    Mercado libre products

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/App.css';
+import { Home } from './Home';
+import { Products } from './Products';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Home></Home>
+      <Products></Products>
+    </div>
+  );
+}
+
+export default App;
